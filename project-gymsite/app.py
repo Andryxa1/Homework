3ce6fc290609fb89fa594ca49d49d0ad08d23b99
Andrew <andrey@gmail.com>
2022-12-29 09:29:20 +0100

    added styles

--- project-gymsite/app.py
+++ project-gymsite/app.py
@@ -5,40 +5,33 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_Dima'
 
-# def get_db_connection():
-#     conn = sqlite3.connect('database.db')
-#     conn.row_factory = sqlite3.Row
-#     return conn
+def get_db_connection():
+    conn = sqlite3.connect('C:\\Users\\Andrew\\Desktop\\courses')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 @app.route('/about')
 def index():
  ##   conn = get_db_connection()
-  ##  result = conn.execute('SELECT * FROM posts').fetchall()
+  ##  result = conn.execute('SELECT * FROM Courses').fetchall()
   ##  conn.close()
     return render_template('about.html')
     
 @app.route('/')
 def home():
- ##   conn = get_db_connection()
-  ##  result = conn.execute('SELECT * FROM posts').fetchall()
-  ##  conn.close()
-    return render_template('about.html')
+   return render_template('about.html')
 
 
 @app.route('/courses')
 def courses():
- ##   conn = get_db_connection()
-  ##  result = conn.execute('SELECT * FROM posts').fetchall()
-  ##  conn.close()
-    return render_template('courses.html')
+   conn = get_db_connection()
+   result = conn.execute('SELECT * FROM Courses').fetchall()
+   conn.close()
+   return render_template('courses.html', courses = result)
 
-# def homepage():
-#   return redirect(url_for('about'))
 
-# @app.route('/home')
-# def homepage():
-#   return render_template('about.html')
+
 @app.route('/gallery')
 def gallery():
  ##   conn = get_db_connection()
